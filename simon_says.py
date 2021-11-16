0cb053f5c57690cdcf3834438a54c37858dc3513
elijahmoline <89874438+elijahmoline@users.noreply.github.com>
2021-11-16 15:25:37 -0600

    Update simon_says.py

--- simon_says.py
+++ simon_says.py
@@ -10,39 +10,42 @@ cp.pixels.brightness = 0.1
 game = numpy.zeros((1, 9))
 num = 0
 
-for i in range(9):                      #generate an array of 10 numbers 0-3
-        num = random.randrange(0, 3, 1)
-        print(num)
-        game[i] = num
+for i in range(9):                      # generate an array of 10 numbers 0-3
+    num = random.randrange(0, 3, 1)
+    print(num)
+    game[i] = num
     
 while True:
     
-    
-    if (game = 0):                    # first quadrant
-        cp.pixels.fill((0, 0, 0))
-        cp.pixels[5] = ((255, 0, 0))
-        cp.pixels[6] = ((255, 0, 0))
-        cp.pixels[7] = ((255, 0, 0))
-        cp.play_tone(262, 1)
-    if (game = 1):                   # second quadrant
-        cp.pixels.fill((0, 0, 0))
-        cp.pixels[7] = ((0, 255, 0))
-        cp.pixels[8] = ((0, 255, 0))
-        cp.pixels[9] = ((0, 255, 0))
-        cp.play_tone(270, 1)    
-    if (game = 2):                   # third quadrant
-        cp.pixels.fill((0, 0, 0))
-        cp.pixels[0] = ((0, 0, 255))
-        cp.pixels[1] = ((0, 0, 255))
-        cp.pixels[2] = ((0, 0, 255))
-        cp.play_tone(278, 1)
-    if (game = 3):                   # fourth quadrant
-        cp.pixels.fill((0, 0, 0))
-        cp.pixels[2] = ((255, 255, 0))
-        cp.pixels[3] = ((255, 255, 0))
-        cp.pixels[4] = ((255, 255, 0))
-        cp.play_tone(288, 1)    
-    
+    for j in range(9):
+        for k in range(j):
+            if (game[k] == 0):                    # first quadrant
+                cp.pixels.fill((0, 0, 0))
+                cp.pixels[5] = ((255, 0, 0))
+                cp.pixels[6] = ((255, 0, 0))
+                cp.pixels[7] = ((255, 0, 0))
+                cp.play_tone(262, 1)
+            if (game[k] == 1):                   # second quadrant
+                cp.pixels.fill((0, 0, 0))
+                cp.pixels[7] = ((0, 255, 0))
+                cp.pixels[8] = ((0, 255, 0))
+                cp.pixels[9] = ((0, 255, 0))
+                cp.play_tone(270, 1)    
+            if (game[k] == 2):                   # third quadrant
+                cp.pixels.fill((0, 0, 0))
+                cp.pixels[0] = ((0, 0, 255))
+                cp.pixels[1] = ((0, 0, 255))
+                cp.pixels[2] = ((0, 0, 255))
+                cp.play_tone(278, 1)
+            if (game[k] == 3):                   # fourth quadrant
+                cp.pixels.fill((0, 0, 0))
+                cp.pixels[2] = ((255, 255, 0))
+                cp.pixels[3] = ((255, 255, 0))
+                cp.pixels[4] = ((255, 255, 0))
+                cp.play_tone(288, 1)
+            
+            
+
     if cp.touch_A1:
         print("Touched A1!")
         cp.pixels.fill((0, 0, 0))
